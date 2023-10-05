d9fbea31ff2903ec83568b6dd3d935eac23c2035
Adam <thomasadam83@hotmail.com>
2023-10-04 22:20:11 -0400

    Added education content

--- src/views/pages/Education/components/index.js
+++ src/views/pages/Education/components/index.js
@@ -1,6 +1,5 @@
-export { default as Systems } from './Systems';
-export { default as Partnership } from './Partnership';
 export { default as Hero } from './Hero';
-export { default as Rules } from './Rules';
 export { default as Counter } from './Counter';
 export { default as Wic } from './Wic';
+export { default as Reason } from './Reason';
+export { default as Technology } from './Technology';
