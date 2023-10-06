42a479f2c07ca6f55c8403fefdc26b17c8efc7e7
Kaverappa KU <caakash357@gmail.com>
2023-10-06 16:43:36 -0400

    adding commission sub pages

---
+++ src/views/pages/CommissionsHumber/components/Partnership/data.js
@@ -0,0 +1,17 @@
+export const partners = [
+  {
+    title: 'Code Adam',
+    image: 'codeadam.jpg',
+    url: 'https://codeadam.ca',
+  },
+  {
+    title: 'Humber College',
+    image: 'humber.jpg',
+    url: 'https://humber.ca',
+  },
+  {
+    title: 'BrevisRefero',
+    image: 'brevisrefero.jpg',
+    url: 'https://brevisrefero.com',
+  },
+];
