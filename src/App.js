7c38df60632533844c435f8056e3245d6ee12ea4
Adam <thomasadam83@hotmail.com>
2023-10-06 07:32:48 -0400

    Added scrollTo on route changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Routes from './Routes';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'aos/dist/aos.css';
+import ScrollToTop from 'common/ScrollToTop';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop></ScrollToTop>
       <Routes />
     </BrowserRouter>
   );
