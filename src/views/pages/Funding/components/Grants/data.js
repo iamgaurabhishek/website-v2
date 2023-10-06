1cb67abb556c3ad58466be645460c65bbd749141
Adam <thomasadam83@hotmail.com>
2023-10-06 12:31:27 -0400

    Added missing comoponents

---
+++ src/views/pages/Funding/components/Grants/data.js
@@ -0,0 +1,17 @@
+export const grants = [
+  {
+    title: 'Cultivate Fund',
+    image: 'cultivate.jpg',
+    url: 'https://humber.ca/research/sotl',
+  },
+  {
+    title: 'Academic Plan Priority 3',
+    image: 'priority-3.jpg',
+    url: 'https://www.humber.ca/academic-division/sites/default/files/documents/Academic%20Plan%20-%2023.02.16.pdf',
+  },
+  {
+    title: 'Greenfield',
+    image: 'greenfield.jpg',
+    url: 'https://humber.ca/research/greenfield',
+  },
+];
