663efb3cc676cb7eb866862ea7b7f412b8e6e730
Shrey patel <shrey@Shreys-MacBook-Air.local>
2023-10-06 13:00:23 -0400

    added basic setup for the commissions page

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from './Home';
 export { default as Systems } from './Systems';
+export { default as Commissions } from './Commissions';
 export { default as Education } from './Education';
 export { default as Funding } from './Funding';
